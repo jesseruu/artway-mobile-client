65ad7066053ef92ae99e12f773d5408bfe55f7da
jdrp20 <jonathanreyes2050@gmail.com>
2022-09-20 20:51:40 -0500

    INSTALCIONES
    Se instala Ionic, Angular y se hace la creacion de la app "Artway"

---
+++ Artway/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Artway',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
