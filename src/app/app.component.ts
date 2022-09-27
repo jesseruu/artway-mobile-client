6eef652da1705eb77e3e5ced5d04cd4b55d8b066
Jesser Lemus <ijesser@outlook.com>
2022-09-27 14:24:31 -0500

    Change status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  constructor() {
+    
+  }
+
+  async ngOnInit() {
+    StatusBar.setOverlaysWebView({ overlay: true });
+    await StatusBar.setStyle({ style: Style.Light });
+  }
+
 }
